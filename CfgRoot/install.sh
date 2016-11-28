5de4d79044094f7759ce010ef14ba7e48baae6e8
Manu-sh <0x4d616e752@protonmail.com>
2016-11-28 22:24:49 +0100

    add dunstrc

--- CfgRoot/install.sh
+++ CfgRoot/install.sh
@@ -1,4 +1,4 @@
-sudo pacman -S sox ranger w3m mutt conky mplayer tmux networkmanager network-manager-applet mupdf feh compton sysstat sxiv ncmpcpp mpv mpc mpd
+sudo pacman -S sox ranger w3m mutt conky mplayer tmux networkmanager network-manager-applet mupdf feh compton sysstat sxiv ncmpcpp mpv mpc mpd dunst
 
 yaourt  -S terminus-font ttf-font-awesome
 
