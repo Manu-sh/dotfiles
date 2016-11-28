e4546a796f9ae086bd58305730bc5ab9ccaeab45
Manu-sh <0x4d616e752@protonmail.com>
2016-11-28 05:43:21 +0100

    little changes

--- CfgRoot/i3/conky_scripts/battery.sh
+++ CfgRoot/i3/conky_scripts/battery.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+styleval() {
+	# assuming that those value are expressed in %
+	[ "$1" -lt 20 ] && echo -e "" "$1" && return 0
+	[ "$1" -lt 50 ] && echo -e "" "$1" && return 0
+	[ "$1" -gt 50 ] && echo -e "" "$1" && return 0
+}
+
 battery() {
 	local battery=$(acpi -b|awk '{print $4}')
-	echo ${battery%%%*}
+	styleval "${battery%%%*}"
 }
 
 battery
--- CfgRoot/i3/conky_scripts/volume.sh
+++ CfgRoot/i3/conky_scripts/volume.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+styleval() {
+        # assuming that those value are expressed in %
+        [ "$1" -lt 5 ] && echo -e "" "$1" && return 0
+        [ "$1" -gt 5 ] && echo -e "" "$1" && return 0
+        [ "$1" -gt 50 ] && echo -e "" "$1" && return 0
+}
 
 volume() {
 	volume=$(amixer get Master|tail -1|cut -sd% -f1)
 	volume=$(sed -u s'@\[@@'g <<<${volume:(-3)})
-	echo $volume
+	styleval "$volume"
 }
 
 volume
