9bfc9807b0152a4de55ee92273164f937a983350
Manu-sh <0x4d616e752@protonmail.com>
2016-11-28 03:19:35 +0100

    mpd basic config

--- CfgRoot/install.sh
+++ CfgRoot/install.sh
@@ -1,5 +1,6 @@
-sudo pacman -S sox ranger w3m mutt conky mplayer tmux network-manager-applet mupdf feh compton sysstat
+sudo pacman -S sox ranger w3m mutt conky mplayer tmux network-manager-applet mupdf feh compton sysstat sxiv ncmpcpp mpv mpc
 
 sudo pacman -S terminus-font tamsyn-font
 
 yaourt -S phallus-fonts-git stlarch_font ttf-font-awesome ttf-font-icons
+
