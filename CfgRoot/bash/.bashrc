e80cf521f0f23f82105c0cbedee65bc2d6ca9f65
Manu-sh <0x4d616e752@protonmail.com>
2016-12-02 16:38:16 +0100

    --

--- CfgRoot/bash/.bashrc
+++ CfgRoot/bash/.bashrc
@@ -375,4 +375,3 @@ regen
 
 #export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7
 #source /usr/bin/virtualenvwrapper.sh
-
--- CfgRoot/common_files/wallpaper/flat/bgset.sh
+++
@@ -1,9 +0,0 @@
-BGDIR=/home/user/Immagini/vaporwawe_wall/flat
-
-# feh --bg-scale $BGDIR/${C}.*
-
-shopt -s direxpand
-
-if [ -e "$BGDIR/00.*" ]; then
-	echo found
-fi
