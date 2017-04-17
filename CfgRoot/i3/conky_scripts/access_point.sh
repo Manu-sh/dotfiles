ea220c70ef5a1586d7665251fd22e4623b69c2a4
manu-sh <0x4d616e752@protonmail.com>
2017-04-17 17:46:14 +0200

    --

--- CfgRoot/i3/conky_scripts/access_point.sh
+++ CfgRoot/i3/conky_scripts/access_point.sh
@@ -6,14 +6,16 @@
 #local iface="$((route -n 2>/dev/null || routel)|grep '^default'|grep -o '[^ ]*$')"
 #echo "$iface, $ap, $ip"
 
-wcl() { sed -u 's/^[ \t]*//;/^$/d'<<<"${1}"|wc -l; }
+isValidStr() {
+	[ "$(sed -u 's/^[ \t]*//;/^$/d'<<<"${1}"|wc -l)" != 0 ] && echo "$1" || return 1
+}
 
 netinfo() {
 	local buf="$(ip route list table 0)"
 	case $1 in
-		gateway) awk '/default via .* dev/{ print $3; exit }'<<<"${buf}" ;;
-		iface) awk '/default via .* dev/{ print $5; exit }'<<<"${buf}" ;;
-		ap) iw dev "$(netinfo "iface")" link 2>/dev/null|grep SSID|cut -d " " -f 2- ;;
+		gateway) isValidStr "$(awk '/default via .* dev/{ print $3; exit }'<<<"${buf}")" ;;
+		iface) isValidStr "$(awk '/default via .* dev/{ print $5; exit }'<<<"${buf}")" ;;
+		ap) isValidStr "$(iw dev "$(netinfo "iface")" link 2>/dev/null|grep SSID|cut -d " " -f 2-)" ;;
 		default) return 1 ;;
 	esac
 }
