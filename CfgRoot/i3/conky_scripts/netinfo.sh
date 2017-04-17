96fff90d2f1612d2ac1012dc2bb3e743fcb73007
manu-sh <0x4d616e752@protonmail.com>
2017-04-17 18:13:22 +0200

    add netinfo

---
+++ CfgRoot/i3/conky_scripts/netinfo.sh
@@ -0,0 +1,39 @@
+isValidStr() {
+	[ "$(sed -u 's/^[ \t]*//;/^$/d'<<<"${1}"|wc -l)" != 0 ] && echo "$1" || return 1
+}
+
+netinfo() {
+	local buf="$(ip route list table 0)"
+	case $1 in
+		gateway) isValidStr "$(awk '/default via .* dev/{ print $3; exit }'<<<"${buf}")" ;;
+		iface) isValidStr "$(awk '/default via .* dev/{ print $5; exit }'<<<"${buf}")" ;;
+		ap) isValidStr "$(iw dev "$(netinfo "iface")" link 2>/dev/null|grep SSID|cut -d " " -f 2-)" ;;
+		default) return 1 ;;
+	esac
+}
+
+case $1 in
+	ap)
+		if AP=$(netinfo "ap"); then
+			echo " $AP"
+		else
+			echo " ethernet"
+		fi
+	;;
+
+	gateway)
+		if IP=$(netinfo "gateway"); then
+			echo "$IP"
+		else
+			echo " ip not found"
+		fi
+	;;
+
+	iface)
+		if IFACE=$(netinfo "iface"); then
+			echo "$IFACE"
+		else
+			echo "$IFACE"
+		fi
+	;;
+esac
