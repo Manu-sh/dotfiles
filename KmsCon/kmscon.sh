b0398445bf03d939e3540a86cdbd55773ad7f1e3
Manu-sh <s3gmentationfaul@gmail.com>
2017-11-24 22:36:07 +0100

    --

--- KmsCon/kmscon.sh
+++ KmsCon/kmscon.sh
@@ -1,8 +1,8 @@
 sudo pacman -S --noconfirm kmscon
+sudo cp -vi *.service /usr/lib/systemd/system || exit
+
 systemctl disable getty@tty1.service
 systemctl enable kmsconvt@tty1.service
+
 sudo ln -s /usr/lib/systemd/system/kmsconvt\@.service /etc/systemd/system/autovt\@.service1
 sudo usermod $USER -aG audio
-
-
-
